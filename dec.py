ea7970f0ffa7847840bbe61d3320e7139d03f5fe
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2024-12-15 06:58:14 +0700

    Update dec.py

--- dec.py
+++ dec.py
@@ -1,7 +1,8 @@
 import marshal, base64
 
-encoded_data = "..."  # Ganti dengan string encoded di script Anda
+encoded_data = input("masukan file path encoding : ")  # Ganti dengan string encoded di script Anda
 decoded_data = marshal.loads(base64.b64decode(encoded_data))
 
 with open("decoded_script.py", "w") as f:
     f.write(decoded_data)
+    print("succes")
